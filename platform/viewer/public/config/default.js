435a659558c4ec622b62499da82aa285600710e3
daewoon kim <dwnusa@gmail.com>
2020-03-12 09:22:32 +0900

    update

--- platform/viewer/public/config/default.js
+++ platform/viewer/public/config/default.js
@@ -7,9 +7,9 @@ window.config = {
     dicomWeb: [
       {
         name: 'dicomWeb_server',
-        wadoUriRoot: 'http://52.231.31.214:5985',
-        qidoRoot: 'http://52.231.31.214:5985',
-        wadoRoot: 'http://52.231.31.214:5985',
+        wadoUriRoot: 'http://52.231.64.176:5985',
+        qidoRoot: 'http://52.231.64.176:5985',
+        wadoRoot: 'http://52.231.64.176:5985',
         imageRendering: 'wadouri',
         thumbnailRendering: 'wadors',
         requestOptions: {
